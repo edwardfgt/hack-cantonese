1b217ddf1dc9554b6434ee0e05cc1a71ee9c858a
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-10-05 17:05:45 +0100

    Install cors package and configure server to allow requests from frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/users");
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(cors({ origin: "http://localhost:8080" }));
 
 app.use("/user", userRoutes);
 
