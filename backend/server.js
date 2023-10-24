f7aeee6b1be1af1fd6662823d853ba96d9c4fe4d
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-10-24 22:43:08 +0100

    Create logger middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/users");
+// const authRoutes = require("./routes/auth");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const { mongoConnect } = require("./services/mongo");
+const { logger } = require("./middleware/logger");
+
+app.use(logger);
 
 app.use(cors({ origin: "http://localhost:8080" }));
 app.use(express.json());
 
 app.use("/user", userRoutes);
+// app.use("/auth", authRoutes);
 
 mongoConnect()
   .then(() => {
